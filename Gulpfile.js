c27c2958bda512631bb10d7382b9bc7ffba76662
Reese Bingham <reese.bingham@aggiemail.usu.edu>
2016-12-19 15:11:30 -0700

    Formatting

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,20 +1,20 @@
 "use strict";
 
 var changed = require('gulp-changed'),
-		ts = require("gulp-typescript"),
-		gulp = require("gulp"),
-		tsProject = ts.createProject('tsconfig.json');
+	ts = require("gulp-typescript"),
+	gulp = require("gulp"),
+	tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('default', ['ts'], function(){
+gulp.task('default', ['ts'], function () {
 	gulp.watch('src/**/*', ['ts']);
 });
 
-gulp.task('ts', function(){
+gulp.task('ts', function () {
 	gulp.src(["./src/**/*.ts", "./typings**/*.d.ts"])
 		.pipe(tsProject())
 		.pipe(gulp.dest("dist/"));
 });
 
-gulp.task('clean', function(){
-	
+gulp.task('clean', function () {
+
 });
