4c0671f5889cc276d6398e0fcf864eb54a68c8e4
Reese Bingham <reese.bingham@aggiemail.usu.edu>
2016-12-19 15:10:38 -0700

    Fixed some issues. Added tsconfig.

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,24 +2,16 @@
 
 var changed = require('gulp-changed'),
 		ts = require("gulp-typescript"),
-		gulp = require("gulp");
+		gulp = require("gulp"),
+		tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('default', ['ts'], function(){
 	gulp.watch('src/**/*', ['ts']);
 });
 
 gulp.task('ts', function(){
-	gulp.src("src/**/*.ts")
-		.pipe(ts({
-			"target": "es6",
-			"module": "commonjs",
-			"moduleResolution": "node",
-			"sourceMap": true,
-			"emitDecoratorMetadata": true,
-			"experimentalDecorators": true,
-			"removeComments": false,
-			"noImplicitAny": false
-		}))
+	gulp.src(["./src/**/*.ts", "./typings**/*.d.ts"])
+		.pipe(tsProject())
 		.pipe(gulp.dest("dist/"));
 });
 
