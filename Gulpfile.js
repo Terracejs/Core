70933a0425e48197162b81272c6e18ec67a2ffc7
Reese Bingham <reese.bingham@aggiemail.usu.edu>
2016-12-19 14:52:52 -0700

    Making some updates.

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,8 +4,23 @@ var changed = require('gulp-changed'),
 		ts = require("gulp-typescript"),
 		gulp = require("gulp");
 
-gulp.task('global', ['ts'], function(){
+gulp.task('default', ['ts'], function(){
 	gulp.watch('src/**/*', ['ts']);
+});
+
+gulp.task('ts', function(){
+	gulp.src("src/**/*.ts")
+		.pipe(ts({
+			"target": "es6",
+			"module": "commonjs",
+			"moduleResolution": "node",
+			"sourceMap": true,
+			"emitDecoratorMetadata": true,
+			"experimentalDecorators": true,
+			"removeComments": false,
+			"noImplicitAny": false
+		}))
+		.pipe(gulp.dest("dist/"));
 });
 
 gulp.task('clean', function(){
