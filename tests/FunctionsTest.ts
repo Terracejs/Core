2c1a0b4e4a806b1d6bfdeccd52de695460cc699c
Reese Bingham <reese.bingham@aggiemail.usu.edu>
2016-12-19 22:01:22 -0700

    Added function tests.

---
+++ tests/FunctionsTest.ts
@@ -0,0 +1,64 @@
+import * as assert from "assert";
+import * as fs from "fs";
+import * as filePath from "path";
+import { public_path, app_path, env, storage_path } from "../src/Functions";
+
+describe("Function Tests", function () {
+	describe("app_path", function () {
+		it("Should return current path", function () {
+			assert.equal(fs.realpathSync(__dirname + "/../"), app_path());
+		});
+	});
+
+	describe("env", function () {
+
+		it("Should return test123 for TEMP_VARIABLE", function () {
+			process.env["TEMP_VARIABLE"] = "test123";
+			assert.equal("test123", env("TEMP_VARIABLE"));
+			delete process.env["TEMP_VARIABLE"];
+		});
+
+		it("Should return default value for missing variable", function () {
+			assert.equal("default", env("MISSING_VARIABLE", "default"));
+		});
+
+		it("Should split comma delimited strings", function () {
+			process.env["TEMP2_VARIABLE"] = "test1,test2,test3";
+			let values = env("TEMP2_VARIABLE");
+			assert.equal(3, values.length);
+			assert.equal("test1", values[0]);
+			assert.equal("test2", values[1]);
+			assert.equal("test3", values[2]);
+			delete process.env["TEMP2_VARIABLE"];
+		});
+
+	});
+
+	describe("storage_path", function () {
+		it("Should return app_path + storage", function () {
+			let path = filePath.normalize(__dirname + "/../storage");
+			assert.equal(path, storage_path());
+		});
+
+		it("Should return app_path + STORAGE_DIR env value", function () {
+			process.env["STORAGE_DIR"] = "/test123";
+			let path = filePath.normalize(__dirname + "/../test123");
+			assert.equal(path, storage_path());
+			delete process.env["STORAGE_DIR"];
+		});
+	});
+
+	describe("public_path", function () {
+		it("Should return app_path + public", function () {
+			let path = filePath.normalize(__dirname + "/../public");
+			assert.equal(path, public_path());
+		});
+
+		it("Should return app_path + PUBLIC_DIR env value", function () {
+			process.env["PUBLIC_DIR"] = "/test123";
+			let path = filePath.normalize(__dirname + "/../test123");
+			assert.equal(path, public_path());
+			delete process.env["PUBLIC_DIR"];
+		});
+	});
+});
