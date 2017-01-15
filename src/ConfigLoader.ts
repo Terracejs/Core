edc1ec53152ea4533b5947059ac7889d4cdcb727
Reese Bingham <reese.bingham@aggiemail.usu.edu>
2017-01-15 01:36:50 -0700

    Mostly finished config loader. Added tests.

--- src/ConfigLoader.ts
+++ src/ConfigLoader.ts
@@ -1,5 +1,6 @@
+import { basename } from "path";
 import { EventEmitter } from "events";
-import { env, app_path, get_files, FileResult } from "./HelperFunctions";
+import { env, config_path, get_files, FileResult } from "./HelperFunctions";
 
 /**
  * Class for loading configs into the
@@ -12,7 +13,7 @@ export default class ConfigLoader extends EventEmitter {
 	/**
 	 * The location of the configuration files
 	 */
-	private _configDir: string = env("CONFIG_DIR", app_path() + "/config");
+	private _configDir: string = "./config"; //config_path();
 
 	/**
 	 * 
@@ -30,7 +31,6 @@ export default class ConfigLoader extends EventEmitter {
 	private constructor() {
 		super();
 		this._configs = new Map<string, any>();
-		this.load();
 	}
 
 	/**
@@ -38,72 +38,112 @@ export default class ConfigLoader extends EventEmitter {
 	 * 
 	 * @fires ConfigLoader#loaded
 	 * @fires ConfigLoader#error
+	 * @returns {Promise<void>} Empty Promise
 	 */
-	private load(): void {
-		this.getFileList()
-			.then(fileList => {
-				return this.readFiles(fileList);
-			})
-			.then(fileData => {
-				for (let config of fileData) {
-					this.readConfig("", fileData);
-				}
-				/**
-				 * Loaded event.
-				 * 
-				 * @event ConfigLoader#loaded
-				 */
-				this.emit("loaded");
-			})
-			.catch(err => {
-				this.emit("error", err);
-			});
+	public async load(): Promise<void> {
+		try {
+			let fileList: Array<FileResult> = await this.getFileList(),
+				errors
+
+			for (let file of fileList) {
+				let data = this.readFile(file.fileName);
+				let namespace = basename(file.fileName).replace(".config.js", "");
+
+				this._configs.set(namespace, data);
+			}
+
+			/**
+			 * Loaded event.
+			 * 
+			 * @event ConfigLoader#loaded
+			 */
+			this.emit("loaded");
+
+			this._loaded = true;
+		} catch (err) {
+			this.emit("error", err);
+		}
 	}
 
 	/**
 	 * Get the list of config files
+	 * 
+	 * @returns {Promise<Array<FileResults>>} 
 	 */
-	private getFileList(): Promise<Array<FileResult>> {
-		return new Promise<Array<FileResult>>((resolve, reject) => {
-
-		});
+	private async getFileList(): Promise<Array<FileResult>> {
+		return await get_files(this._configDir, /\.config\.js/);
 	}
 
 	/**
 	 * Read the list of files and pull their data
+	 * 
+	 * @param {FileResult} fileList The list of files to read
+	 * 
+	 * @returns {Object} The data in the config files
 	 */
-	private readFiles(fileList: Array<FileResult>): Promise<Array<Object>> {
-		return new Promise((resolve, reject) => {
-			let results = [];
-
-			try {
-				for (let file of fileList) {
-					results.push(require(file.filePath));
-				}
-
-				resolve(results);
-			} catch (e) {
-				reject(e);
-			}
-
-		});
+	private readFile(fileName: string): Object {
+		return require(fileName);
 	}
 
 	/**
-	 * Parse the config data
+	 * Get the value from the config
+	 * 
+	 * @param {any} obj Either the object to find the value or the value
+	 * @param {Array<string>} path The namespace path to the value
+	 * @param {number} index The current index in the path
+	 * 
+	 * @throws {TypeError}
+	 * 
+	 * @returns The value of the config
 	 */
-	private readConfig(namespace: string, obj: Object): void {
-		for (let i in obj) {
-			if (typeof obj[i] === "object" && !Array.isArray(obj[i])) {
-				this.readConfig(`${namespace}.${i}`, obj[i]);
+	private getValue(obj: any, path: Array<string>, index: number): any {
+		if (index === path.length) {
+			return obj;
+		} else {
+
+			if (obj instanceof Object) {
+
+				if (obj instanceof Array) {
+					let tempIndex = Number.parseInt(path[index]);
+
+					if(!Number.isNaN(tempIndex) && (<Array<any>>obj).length > tempIndex){
+						
+						return this.getValue((<Array<any>>obj)[tempIndex], path, index + 1);
+
+					} else {
+						throw new TypeError(`Index must be an integer: ${index}`);
+					}
+					
+				} else if((<Object>obj).hasOwnProperty(path[index])){
+					
+					return this.getValue((<Object>obj)[path[index]], path, index + 1);
+
+				} else {
+					throw new TypeError(`Path can't be followed: ${path.join(".")}`);
+				}
+
 			} else {
-				this._configs.set(`${namespace}.${i}`);
+				throw new TypeError(`Path can't be followed: ${path.join(".")}`);
 			}
+
 		}
 	}
 
-	public get(name: string): any {
-		return this._configs.get(name);
+	/**
+	 * @param {string} name Config name to get
+	 * 
+	 * @throws {ReferenceError|TypeError}
+	 * @returns {any}
+	 */
+	public get(name: string, value?: any): any {
+		let namespace = name.split("."),
+			obj = namespace[0];
+
+		if(this._configs.has(obj)) {
+			return this.getValue(this._configs.get(obj), namespace, 1);
+		} else {
+				throw new ReferenceError(`${obj} doesn't exists`);
+		}
 	}
 
 	/**
@@ -120,6 +160,8 @@ export default class ConfigLoader extends EventEmitter {
 
 	/**
 	 * Get the ConfigLoader instance
+	 * 
+	 * @returns {ConfigLoader} Config loader instance
 	 */
 	static get Instance(): ConfigLoader {
 		if (this._instance === undefined) {
---
+++ src/tests/ConfigLoader.test.ts
@@ -0,0 +1,81 @@
+import * as assert from "assert";
+import * as mock_fs from "mock-fs";
+import * as fs from "fs";
+import * as mocha from 'mocha';
+import ConfigLoader from "../ConfigLoader";
+
+
+describe("ConfigLoader Tests", function () {
+	before(function () {
+		mock_fs({
+			'config': {
+				'test1.config.js': "module.exports = {};",
+				'Services': {
+					'test2.config.js': 'module.exports = {};'
+				}
+			},
+			'app': {/** another empty directory */ }
+		});
+	});
+
+	after(function () {
+		mock_fs.restore();
+	});
+
+	describe("Instance", function () {
+		it("Returns an instance of the class", function () {
+			assert.equal(true, ConfigLoader.Instance instanceof ConfigLoader);
+		});
+	});
+
+	describe("getFileList", function () {
+		it("returns all .config.js files in config dir", async function () {
+			let files = await ConfigLoader.Instance["getFileList"]();
+			assert.equal(2, files.length);
+		});
+	});
+
+	describe("getValue", function () {
+		it("Errors on invalid path", function () {
+			let config = ConfigLoader.Instance;
+
+			assert.throws(function () {
+				config["getValue"](1, ["abc", "def"], 0);
+			});
+		});
+
+		it("Errors if object doesn't have property", function () {
+			let config = ConfigLoader.Instance;
+
+			assert.throws(function () {
+				let obj = {};
+				config["getValue"](obj, ["abc", "def"], 0);
+			});
+		});
+
+		it("Returns the appropriate value on object", function () {
+			let config = ConfigLoader.Instance,
+				obj = { testValue: "123" };
+
+			assert.equal(obj.testValue, config["getValue"](obj, ["obj", "testValue"], 1));
+		});
+
+		it("Errors if entering an array without number index", function () {
+			let config = ConfigLoader.Instance,
+				obj = ["123", "456"];
+
+			assert.throws(function () {
+				config["getValue"](obj, ["asdf", "asdf"], 1);
+			});
+		});
+
+		it("Returns the appropriate value on array", function () {
+			let config = ConfigLoader.Instance,
+				obj = ["123", "456"];
+			
+			assert.equal(obj[0], config["getValue"](obj, ["", "0"], 1));
+		});
+	});
+
+
+});
