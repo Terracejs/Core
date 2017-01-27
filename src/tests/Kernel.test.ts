fb6048d5899ef93c4f14b5d2901147dd980ead59
Reese Bingham <reese.bingham@aggiemail.usu.edu>
2017-01-27 01:37:48 -0700

    Some small fixes. Implements tests for LoadService

--- src/tests/Kernel.test.ts
+++ src/tests/Kernel.test.ts
@@ -2,6 +2,25 @@ import * as assert from "assert";
 import { Mock, Times } from "typemoq";
 import Kernel from "../Kernel";
 import ConfigLoader from "../ConfigLoader";
+import { IService } from "../Services/IService";
+
+class MockService implements IService {
+	public async Start(): Promise<boolean> {
+		return true;
+	}
+
+	public async Stop(): Promise<boolean> {
+		return true;
+	}
+
+	public get Running(): boolean {
+		return false;
+	}
+
+	public get Name(): string {
+		return "MockService";
+	}
+}
 
 describe("Kernel tests", function () {
 	describe("Instance", function () {
@@ -20,10 +39,11 @@ describe("Kernel tests", function () {
 				.callback(() => { config.object.emit("loaded"); })
 				.verifiable(Times.once());
 
-			kernel["initialize"](config.object);
+			kernel["Initialize"](config.object);
 
 			config.verifyAll();
 			assert.equal(true, kernel.initialized);
+			kernel["_initialized"] = false;
 		});
 
 		it("Errors when ConfigLoader errors", function () {
@@ -35,10 +55,32 @@ describe("Kernel tests", function () {
 				.callback(() => { config.object.emit("error"); })
 				.verifiable(Times.once());
 
-			kernel["initialize"](config.object);
+			kernel["Initialize"](config.object);
 
 			config.verifyAll();
 			assert.equal(false, kernel.initialized);
+		});
+	});
+
+	describe("LoadService", function () {
+		it("Creates new instance of the service class", function () {
+			let kernel = Kernel.Instance;
+			let reqFun = function (id: string): any {
+				return MockService;
+			}
+
+			let service: IService = kernel["LoadService"](reqFun, { name: "Test" });
+			assert.equal(true, service !== undefined);
+			assert.equal(true, service instanceof MockService);
+		});
+
+		it("Errors on loading the service class", function () {
+			let kernel =  Kernel.Instance;
+			let reqFun = function (id: string): any {
+				throw new Error("This should be thrown");
+			}
+
+			assert.throws(kernel["LoadService"]);
 		});
 	});
 });
